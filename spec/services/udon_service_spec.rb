3b83359774c4ea8b4c6e4d0518f99d78cda53c8d
Alyson Takai <shtakai@gmail.com>
2018-12-27 07:37:10 +0700

    Add spec/actual class

---
+++ spec/services/udon_service_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe UdonService do
+  describe 'fuck' do
+    it 'is okay' do
+      expect(2 + 2).to eq 4
+    end
+  end
+end
