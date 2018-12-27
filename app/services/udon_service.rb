818809d3d45b3785077137d703f980d71cc76952
Alyson Takai <shtakai@gmail.com>
2018-12-27 08:13:41 +0700

    Add travel_to testing normal

--- app/services/udon_service.rb
+++ app/services/udon_service.rb
@@ -1,5 +1,21 @@
 #frozen_string_literal: true
 
 class UdonService
+  UdonTimeError = Class.new(StandardError)
+
+  def perform!
+    ensure_time
+  end
+
+  private
+
+  def ensure_time
+    puts "current_time: #{current_time}"
+    raise UdonTimeError if current_time.hour == 0
+  end
+
+  def current_time
+    Time.zone.now
+  end
 
 end
--- spec/services/udon_service_spec.rb
+++ spec/services/udon_service_spec.rb
@@ -3,9 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe UdonService do
-  describe 'fuck' do
-    it 'is okay' do
-      expect(2 + 2).to eq 4
+  let(:subject) { described_class.new }
+
+  describe '#perform!' do
+    it 'respond to #perform!' do
+      expect(subject).to respond_to(:perform!)
+    end
+
+    context('current hour is other than 0') do
+      it 'works wo/error' do
+        (1..23).to_a.each do |hour|
+          travel_to Time.zone.now.change(hour: hour, sec: 1)
+          expect{ subject.perform! }.not_to raise_error
+        end
+      end
+    end
+
+    context('current hour is 0') do
+      it 'works wo/error' do
+        travel_to Time.zone.now.change(hour: 0, sec: 1)
+        expect{ subject.perform! }.to raise_error UdonService::UdonTimeError
+      end
     end
   end
 end
